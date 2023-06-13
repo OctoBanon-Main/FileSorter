a52af94b61ac66e4ac062a8c5a1b3cd0d44bbd25
OctoBanon <octobanon@gmail.com>
2023-06-13 18:26:41 +0700

    Creating directories

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::create_dir_all;
+
+fn create_directories() {
+    let directories = vec![
+        "Audio",
+        "Videos",
+        "Photos",
+        "Icons",
+        "Archives",
+        "Other",
+        "Executable",
+        "Disk Images",
+        "3D Models"
+    ];
+
+    for directory in &directories {
+        if let Err(err) = create_dir_all(directory) {
+            eprintln!("Failed to create directory {}: {}", directory, err);
+        } else {
+            println!("Created directory: {}", directory);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    create_directories();
 }
